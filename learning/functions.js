57e056c758424a19cccb66504f5c6c8f4e36564f
Sidumiso24 <sidumisodebbie@gmail.com>
2024-02-04 23:41:45 +0200

    Add node modules and configurations, creat a functions file and test file

---
+++ learning/functions.js
@@ -0,0 +1,9 @@
+// create a function that greets user and displays 'Hello there, friend'
+function greetUser() {
+    greeting = "Hello there, friend!";
+    return greeting;
+}
+
+module.exports = greetUser;
+
+console.log(greetUser());
---
+++ learning/functions_test.js
@@ -0,0 +1,3 @@
+test('greeting user message', () => {
+    expect(greetUser()).toBe("Hello there, friend!");
+});

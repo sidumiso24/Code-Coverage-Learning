41df03b289a5ff48dfadfcb7ca8a91b7440e0b82
Sidumiso24 <sidumisodebbie@gmail.com>
2024-02-07 15:45:43 +0200

    Adding more tests and functions

--- __tests__/main.spec.js
+++ __tests__/main.spec.js
@@ -13,3 +13,34 @@ describe("Function", () => {
         expect(greetUser()).toBe("Hello there, friend!");
     });
 })
+
+
+// function to check the factorial of parsed number
+function factorialize(num) {
+    if (num < 0) return -1;
+    else if (num == 0) return 1;
+    else {
+        return num * factorialize(num - 1);
+    }
+}
+
+// const { factorialize } = require("./maths");
+
+
+// test("factorial of 3", () => {
+//     expect(factorialize(3)).toBe(6);
+// });
+
+// test("factorial of 5", () => {
+//     expect(factorialize(5)).toBe(120);
+// });
+
+test("factorial of 3 is not 5", () => {
+    expect(factorialize(3)).not.toBe(5);
+});
+
+
+function greetUserWithName(username) {
+    greeting = "Hello there, " + username + "!";
+    return greeting;
+}
